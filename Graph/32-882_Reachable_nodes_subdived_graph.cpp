ef522549fe3f6bd8523de394896759dea76ce9a7
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2022-06-14 17:44:16 +0530

    Graph 8

--- Graph/32-882_Reachable_nodes_subdived_graph.cpp
+++ Graph/32-882_Reachable_nodes_subdived_graph.cpp
@@ -33,13 +33,51 @@ int dfs_u(int src,int cur_cunt ,vector<bool>&visisted ,vector<vector<int>> *grap
 
 int reachableNodes(vector<vector<int>>& edges, int maxMoves, int n) 
 {
-    vector<vector<int>> *graph1 = new vector<vector<int>>[n]   ;
-    for (auto x: edges)
-        graph1[x[0]].push_back({x[1],x[2]}) ,graph1[x[1]].push_back({x[0],x[2]})    ;
+    int ans1 = 0,mpoves ,nde;
+    unordered_map<int,unordered_map<int,int>>edge_list_that_saves_weight ;
+    for (auto e:edges)
+        edge_list_that_saves_weight[e[1]][e[0]] = edge_list_that_saves_weight[e[0]][e[1]] = e[2];
 
-    vector<bool>visisted(n,false);
-    int coubht = dfs_u(0,maxMoves,visisted,graph1);
-    return coubht ;
+    priority_queue<pair<int,int>>pq_var ; //moves,nodes //sort by nodes
+    pq_var.push({maxMoves,0});
+
+    unordered_map<int,int>visited ;
+
+    while (pq_var.size())
+    {
+        mpoves = get<0>(pq_var.top()) ; 
+        nde = get<1>(pq_var.top()) ; 
+
+        // int nde = pq_var.top().second ; 
+        pq_var.pop();
+
+        if(visited.find(nde)==visited.end())
+        {
+            visited[nde] = mpoves ;
+            for(auto neig:edge_list_that_saves_weight[nde])
+            {
+                int newmpoves = mpoves-neig.second-1 ;//inlcuding node //neig is node, weight
+                if(visited.find(neig.first)==visited.end() && newmpoves>=0)
+                    pq_var.push({newmpoves, neig.first });
+            }
+        }
+    }
+
+    ans1 = visited.size();//theses nodes were reached .
+    for(auto ed : edges)
+    {
+        int firside = 0,secondside = 0;
+        if(visited.find(ed[0])!=visited.end())
+            firside = visited[ed[0]];//moves
+        if(visited.find(ed[1])!=visited.end())
+            secondside = visited[ed[1]];//moves
+        
+        ans1+=min(firside+secondside,ed[2]);//if the weight cover the edge from both side 
+    }
+
+
+    return ans1;
+
 
 }
 
