de309f3a6e1d787596bc9d1cd8e52e1ee6c84ea9
Neem Singh <Stenardt-9002>
2022-06-25 23:23:21 +0530

    Array 5

--- Array/16-40-Combination_sum2.cpp
+++ Array/16-40-Combination_sum2.cpp
@@ -11,7 +11,34 @@ using namespace __gnu_pbds ;
 typedef  long long ll1d;
 
 
+
+void recursive_util(int ind , int target , vector<int> candy , vector<int> &ds ,vector<vector<int>> &ans1)
+{
+  if(ind==candy.size())
+  {
+    if(target==0)
+      ans1.push_back(ds);
+    return ;
+  }
+
+
+  if(target>=candy[ind])
+  {
+    ds.push_back(candy[ind]);
+    recursive_util(ind+1,target-candy[ind],candy,ds,ans1);
+    ds.pop_back();
+  }
+  recursive_util(ind+1,target,candy,ds,ans1);
+}
+
+
 vector<vector<int>> combinationSum2(vector<int>& candidates, int target)
 {
-  
+  int n = A.size();
+  vector<vector<int>> ans1 ;
+  vector<int> s1 ;
+  sort(A.begin(),A.end());
+  A.erase(unique(A.begin(),A.end()),A.end() );
+  recursive_util(0 , B , A , s1 , ans1) ; //index , target , array , stack , list of list
+  return ans1 ;
 }
